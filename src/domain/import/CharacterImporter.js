9a40be25d57de6b28bd2e3b6f0b480f1dba3474c
BaHalus <84689433+BaHalus@users.noreply.github.com>
2026-06-18 23:33:51 -0300

    feat: integrate traits importer into character importer

--- src/domain/import/CharacterImporter.js
+++ src/domain/import/CharacterImporter.js
@@ -5,12 +5,14 @@ import {
   importAttributes,
   importSecondaryCharacteristics,
 } from "./importers/AttributesImporter.js";
+import { importTraits } from "./importers/TraitsImporter.js";
 
 export function createSnapshotFromGcs(source = {}) {
   return createImportSnapshot({
     identity: importIdentity(source),
     attributes: importAttributes(source),
     secondaryCharacteristics: importSecondaryCharacteristics(source),
+    traits: importTraits(source),
     raw: source,
   });
 }
@@ -22,5 +24,10 @@ export function importCharacter(source = {}) {
     identity: snapshot.identity,
     attributes: snapshot.attributes,
     secondaryCharacteristics: snapshot.secondaryCharacteristics,
+
+    advantages: snapshot.traits.advantages,
+    perks: snapshot.traits.perks,
+    disadvantages: snapshot.traits.disadvantages,
+    quirks: snapshot.traits.quirks,
   });
 }
