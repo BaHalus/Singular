77067305c07d21933c03fe6e7a61a8f6ccb7f2bf
BaHalus <84689433+BaHalus@users.noreply.github.com>
2026-06-21 09:29:19 -0300

    test: iniciar testes de custo-base

---
+++ src/domain/character/TraitBaseCostCalculator.test.js
@@ -0,0 +1,5 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+
+import { createTraitPointValue } from "./TraitPointValue.js";
+import { calculateTraitBaseCost } from "./TraitBaseCostCalculator.js";
