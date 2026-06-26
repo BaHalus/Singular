aec5f2d0dbc7b22fe3a99f203589af5d19b0f11a
BaHalus <84689433+BaHalus@users.noreply.github.com>
2026-06-26 17:53:11 -0300

    test(app-attacks): adicionar imports

--- src/application/attacks/AttackCommands.test.js
+++ src/application/attacks/AttackCommands.test.js
@@ -1,1 +1,11 @@
-// APP-ATTACK 1.0 tests
+import test from "node:test";
+import assert from "node:assert/strict";
+
+import { createCharacter } from "../../domain/character/Character.js";
+import { executeCommand } from "../commands/CommandExecutor.js";
+import { createCommandRegistry } from "../commands/CommandRegistry.js";
+import { createApplicationSession } from "../session/ApplicationSession.js";
+import {
+  ATTACK_COMMAND_TYPES,
+  createAttackCommandHandlerEntries,
+} from "./AttackCommandHandlers.js";
