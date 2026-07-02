b44b4a993b99353f5a3d0f4cd80c440d130a4f0f
BaHalus <84689433+BaHalus@users.noreply.github.com>
2026-07-02 19:20:09 -0300

    Remove legacy attack string injection path

--- src/ui/mobile/CharacterMobileAttackEditApp.js
+++ src/ui/mobile/CharacterMobileAttackEditApp.js
@@ -17,7 +17,6 @@ import {
   mountCharacterMobileLanguageCultureEditApp,
 } from "./CharacterMobileLanguageCultureEditApp.js";
 import {
-  appendInlineEditorToDefinitionListItem,
   appendInlineEditorToDefinitionListItemNode,
   escapeAttribute,
   escapeSelectorValue,
@@ -158,15 +157,6 @@ export function mountCharacterMobileAttackEditApp(app, options = {}) {
   });
 }
 
-export function injectMobileAttackEditControls(html, character, mode) {
-  if (mode !== "creation") return html;
-  let nextHtml = html;
-  for (const attack of character.attacks ?? []) {
-    nextHtml = appendInlineEditorToAttack(nextHtml, attack);
-  }
-  return nextHtml;
-}
-
 function injectCurrentAttackControls(root, { character, mode, modeSync }) {
   if (mode !== "creation") {
     modeSync.sync();
@@ -181,15 +171,6 @@ function injectCurrentAttackControls(root, { character, mode, modeSync }) {
 
 function appendAttackInlineEditorNode(root, attack) {
   return appendInlineEditorToDefinitionListItemNode(root, {
-    entityId: attack.id,
-    markerAttribute: "data-attack-id",
-    editorRole: "attack-inline-editor",
-    renderEditor: () => renderAttackInlineEditor(attack),
-  });
-}
-
-function appendInlineEditorToAttack(html, attack) {
-  return appendInlineEditorToDefinitionListItem(html, {
     entityId: attack.id,
     markerAttribute: "data-attack-id",
     editorRole: "attack-inline-editor",
